447f3125c0f3e6e2f3b9b2a3d2e7357f31a02158
Delrhon <dfnd@live.com>
2013-07-10 21:55:37 -0700

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var index_html = fs.readFileSync('index.html','utf8');
-console.log(index_html);
 
 app.get('/', function(request, response) {
   response.send(index_html);
